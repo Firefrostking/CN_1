8f4f62351996f0c1b80fbaca9af15f061ac9f25c
Aman Gupta <aupta2002@gmail.com>
2023-04-09 20:16:40 +0530

    error free

--- client_udp.py
+++ client_udp.py
@@ -1,13 +1,13 @@
 from socket import *
 cs = socket(AF_INET,SOCK_DGRAM)
-cs.connect((gethostname(),1240))
+add = (gethostname(),1240)
 while True:
     in1 = input("Enter Text: ")
     if in1 == "over":
-        cs.sendto(in1.encode())
+        cs.sendto(in1.encode(), add)
         break
     else:
-        cs.sendto(in1.encode())
-        sm = cs.recvfrom(1024)
+        cs.sendto(in1.encode(), add)
+        sm, addr = cs.recvfrom(1024)
         print("Server:",sm.decode())
 cs.close()
--- server_udp.py
+++ server_udp.py
@@ -1,14 +1,13 @@
 from socket import *
 ss = socket(AF_INET,SOCK_DGRAM)
+ad1 = ("",1240)
 ss.bind(("",1240))
-ss.listen(5)
-conn,add = ss.accept()
 while True:
-    in1 = conn.recvfrom(1024)
+    in1, add = ss.recvfrom(1024)
     if in1.decode() == "over":
         break
     print("Client: ",in1.decode())
     on1 = input("Enter msg: ")
-    conn.sendto(on1.encode())
+    ss.sendto(on1.encode(), add)
     print("Sent")
-conn.close()
+ss.close()
